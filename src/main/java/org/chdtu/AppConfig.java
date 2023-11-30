5aebb0d19334674603e68b16c7e076856657f90d
Animeshka39 <sergiosasa9@gmail.com>
2023-11-30 18:28:35 +0200

    @EnableAspectJAutoProxy was did`t add in AppConfig.java

--- src/main/java/org/chdtu/AppConfig.java
+++ src/main/java/org/chdtu/AppConfig.java
@@ -2,11 +2,13 @@ package org.chdtu;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("org.chdtu")
 @PropertySource("classpath:myApp.properties")
+@EnableAspectJAutoProxy
 public class AppConfig {
 
 }
--- src/main/java/org/chdtu/Car.java
+++ src/main/java/org/chdtu/Car.java
@@ -1,7 +1,5 @@
 package org.chdtu;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 public interface Car {
     String getName();
